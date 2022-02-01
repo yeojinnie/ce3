9ecf0799cf097c25f8d6131bf498f86537f2979a
yeojinnie <yuyeojin0876@gmail.com>
2022-02-01 11:03:18 -0500

    Initial Upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//When loading is finished, triger start
+
+$(window).on("load", start);
+
+
+function start(){
+    $("ul nav a:first").click();
+}
+
+$(document).on("click","ul.nav a", nav);
+
+function nav(e) {
+    e.preventDefault()
+    var url = $(this).attr("href");
+    $("#content").load(url);
+    $("ul.nav a").removeClass("active");
+    $(this).addClass("active");
+}
